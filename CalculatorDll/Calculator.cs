2a64d9e6f241bf94c6a251d214f1a699633bb885
novruzov_t <novruzov_t@itstep.edu.az>
2023-03-04 11:04:40 +0400

    Add project files.

---
+++ CalculatorDll/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace CalculatorDll;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
---
+++ Source/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+// .dll and .exe
+
+#nullable disable
+
+
+const string dllFile = "CalculatorDll.dll";
+var path = Path.Combine(Directory.GetCurrentDirectory(), "ext", dllFile);
+
+
+if (!File.Exists(path))
+{
+    Console.WriteLine("Error: CalculatorDLL.dll not found");
+    return;
+}
+
+
+
+var assembly = Assembly.LoadFrom(path);
+
+var calculatorType = assembly.GetType("CalculatorDll.Calculator");
+
+var calculatorInstance = Activator.CreateInstance(calculatorType);
+
+var subtractMethodInfo = calculatorType.GetMethod("Subtract");
+var result = subtractMethodInfo.Invoke(calculatorInstance, new object[] { 10, 3 });
+
+Console.WriteLine(result);
+
+
+
+
+
+// Calculator calculator = new();
+// Console.WriteLine(calculator.Add(10, 5));
